60f6c6ac6019c43691c3f52d67e029b2e5c08e72
Edmyr <127021800+edmyrbatiancila@users.noreply.github.com>
2024-05-11 06:20:29 +0800

    added the script.js and adjusted the code.

---
+++ script.js
@@ -0,0 +1,42 @@
+function getAverage(scores) {
+    var average = 0;
+    for(let i = 0; i < scores.length; i++) {
+        average += scores[i];
+    }
+    return average / scores.length;
+}
+
+function getGrade(score) {
+    if(score === 100) {
+        return "A++";
+    } else if(score >= 90 || score === 99 ) {
+        return "A";
+    } else if(score >= 80 || score === 89) {
+        return "B";
+    } else if(score >= 70 || score === 79) {
+        return "C";
+    } else if(score >= 60 || score === 69) {
+        return "D";
+    } else {
+        return "F";
+    }
+}
+
+function hasPassingGrade(score) {
+    
+    if(getGrade(score) !== "F") {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function studentMsg(totalScores, studentScore) {
+    if(getGrade(getAverage(totalScores)) !== "F") {
+        return "Class average: " + getAverage(totalScores) + ". Your grade: " + getGrade(getAverage(totalScores)) + ". You passed the course";
+    } else {
+        return "Class average: " + getAverage(totalScores) + ". Your grade: " + getGrade(getAverage(totalScores)) + ". You failed the course";
+    }
+}
+
+console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
